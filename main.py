44706a33fdb04b0a34db60d4842980ea33e595ea
iAlphaRaptor <maximwaring@yahoo.co.uk>
2021-02-06 22:59:06 +0000

    Pieces can be dragged to move to possible spaces

--- main.py
+++ main.py
@@ -6,18 +6,50 @@ pygame.display.set_caption("Chess")
 clock = pygame.time.Clock()
 
 playing = True
+moves = []
 
 piecesGroup = pygame.sprite.Group()
-piecesGroup.add(piece.Pawn("white", (0, 3)),
-				piece.Rook("white", (2, 5)),
-				piece.King("black", (3, 3)),
-				piece.Queen("white", (4, 2)),
-				piece.Knight("black", (3, 5)))
+piecesGroup.add(piece.Pawn("white", [0, 3]),
+				piece.Bishop("white", [2, 5]),
+				piece.Knight("black", [3, 5]),
+				piece.Queen("white", [4, 4]), 
+				piece.King("black", [2, 2]),
+				piece.Rook("white", [6, 4]))
+
+board = [[" " for x in range(9)] for y in range(9)]
+for piece in piecesGroup.sprites():
+	board[piece.coords[0]][piece.coords[1]] = piece.piece
 
 while playing:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			playing = False
+		elif event.type == pygame.MOUSEBUTTONDOWN:
+			mousex, mousey = pygame.mouse.get_pos()
+			for piece in piecesGroup.sprites():
+				piece.isClicked(mousex, mousey)
+		elif event.type == pygame.MOUSEMOTION and pygame.mouse.get_pressed()[0]:
+			mousex, mousey = pygame.mouse.get_pos()
+			for piece in piecesGroup.sprites():
+				if piece.active:
+					piece.rect.x = mousex - SQUAREWIDTH / 2
+					piece.rect.y = mousey - SQUAREWIDTH / 2
+		elif event.type == pygame.MOUSEBUTTONUP:
+			mousex, mousey = pygame.mouse.get_pos()
+			for piece in piecesGroup.sprites():
+				if piece.active:
+					if (mousex // SQUAREWIDTH, mousey // SQUAREWIDTH) in piece.getMoves(board):
+						board[piece.coords[0]][piece.coords[1]] = " "
+
+						piece.coords[0] = mousex // SQUAREWIDTH
+						piece.coords[1] = mousey // SQUAREWIDTH
+						piece.rect.x = piece.coords[0] * SQUAREWIDTH
+						piece.rect.y = piece.coords[1] * SQUAREWIDTH
+
+						board[piece.coords[0]][piece.coords[1]] = piece.piece
+					else:
+						piece.rect.x = piece.coords[0] * SQUAREWIDTH
+						piece.rect.y = piece.coords[1] * SQUAREWIDTH
 
 	screen.fill((255, 255, 255))
 
@@ -30,9 +62,13 @@ while playing:
 	for i in range(1, 33, 2):
 		pygame.draw.rect(screen, LIGHTCOLOUR, ((i%8)*SQUAREWIDTH, (i//8)*2*SQUAREWIDTH+SQUAREWIDTH, SQUAREWIDTH, SQUAREWIDTH))
 
-	for m in piecesGroup.sprites()[4].possibleMoves:
-		pygame.draw.rect(screen, (0, 0, 255), (m[0]*SQUAREWIDTH, m[1]*SQUAREWIDTH, 10, 10))
+	for p in piecesGroup.sprites():
+		if p.active:
+			moves = p.getMoves(board)
 	piecesGroup.draw(screen)
+
+	for m in moves:
+		pygame.draw.rect(screen, (0, 0, 255), (m[0]*SQUAREWIDTH + SQUAREWIDTH/2, m[1]*SQUAREWIDTH + SQUAREWIDTH/2, 10, 10))
 	pygame.display.flip()
 	clock.tick(60)
 
--- piece.py
+++ piece.py
@@ -28,8 +28,10 @@ class Piece(pygame.sprite.Sprite):
 	def __init__(self, piece, colour, coords):
 		super().__init__()
 
+		self.piece = piece
 		self.colour = colour
 		self.coords = coords
+		self.active = False
 
 		self.image = pygame.Surface([SQUAREWIDTH, SQUAREWIDTH])
 		self.image.set_colorkey((2, 2, 2))
@@ -41,41 +43,62 @@ class Piece(pygame.sprite.Sprite):
 
 		self.image.blit(blackPieces[piece] if colour == "black" else whitePieces[piece], (0,0))
 
-	def getStraightMoves(self, limit=False):
+	def isClicked(self, mousex, mousey):
+		if self.rect.collidepoint(mousex, mousey):
+			self.active = True
+			return True
+		self.active = False
+		return False
+
+	def getStraightMoves(self, board, limit=7):
+		""" Gets all possible moves in the horizontal and vertical directions from a given piece.
+		Returns a list of tuples, representing the co-ords which can be moved to."""
+		directions = [(1, 0), (-1, 0), (0, 1), (0, -1)]
 		moves = []
-		if not limit:
-			for x in range(0, 8):
-				if x != self.coords[0]:
-					moves.append((x, self.coords[1]))
-			for y in range(0, 8):
-				if y != self.coords[1]:
-					moves.append((self.coords[0], y))
-		else:
-			for x in range(self.coords[0]-limit, self.coords[0]+limit+1):
-				if x != self.coords[0]:
-					moves.append((x, self.coords[1]))
-			for y in range(self.coords[1]-limit, self.coords[1]+limit+1):
-				if y != self.coords[1]:
-					moves.append((self.coords[0], y))
+
+		for d in directions:
+			distance = 1
+			nextMove = (self.coords[0] + distance*d[0], self.coords[1] + distance*d[1])
+			nextSquare = board[nextMove[0]][nextMove[1]]
+			while distance < limit + 1 and nextSquare == " " and self.coords[0] + distance*d[0] in range(-1, 9) and self.coords[1] + distance*d[1] in range (-1, 9):
+				moves.append(nextMove)
+
+				nextMove = (self.coords[0] + distance*d[0], self.coords[1] + distance*d[1])
+				nextSquare = board[nextMove[0]][nextMove[1]]
+				distance += 1
+			if nextSquare != " ":
+				moves.append(nextMove)
 
 		return moves
 
-	def getDiagonalMoves(self, limit=False):
+	def getDiagonalMoves(self, board, limit=False):
+		""" Gets all possible moves in the diagonals from a given piece.
+		Returns a list of tuples, representing the co-ords which can be moved to."""
+		possibleDirections = [(1,1), (-1,1), (1,-1), (-1,-1)]
 		moves = []
 		if not limit:
 			for i in range(1, min([7-self.coords[0], 7-self.coords[1]]) + 1):
 				moves.append((self.coords[0] + i, self.coords[1] + i))
+				if board[self.coords[0] + i][self.coords[1] + i] != " ":
+					break
 			for i in range(1, min([7-self.coords[0], self.coords[1]]) + 1):
 				moves.append((self.coords[0] + i, self.coords[1] - i))
+				if board[self.coords[0] + i][self.coords[1] - i] != " ":
+					break
 			for i in range(1, min([self.coords[0], self.coords[1]]) + 1):
 				moves.append((self.coords[0] - i, self.coords[1] - i))
+				if board[self.coords[0]- i][self.coords[1] - i] != " ":
+					break
 			for i in range(1, min([self.coords[0], 7-self.coords[1]]) + 1):
 				moves.append((self.coords[0] - i, self.coords[1] + i))
+				if board[self.coords[0] - i][self.coords[1] + i] != " ":
+					break
 		else:
-			dir = [(1,1), (-1,1), (1,-1), (-1,-1)]
 			for i in range(4):
 				for j in range(1, limit+1):
-					moves.append((self.coords[0]+(dir[i][0]*j), (self.coords[1]+(dir[i][1]*j))))
+					move = (self.coords[0]+(possibleDirections[i][0]*j), (self.coords[1]+(possibleDirections[i][1]*j)))
+					if board[move[0]][move[1]] == " ":
+						moves.append(move)
 
 		return moves
 
@@ -84,37 +107,48 @@ class Pawn(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Pawn", colour, coords)
 
-		self.possibleMoves = [(self.coords[0], self.coords[1] + 1 if self.colour == "black" else self.coords[1] - 1)]
+	def getMoves(self, board):
+		possible = (self.coords[0], self.coords[1] + 1 if self.colour == "black" else self.coords[1] - 1)
+		return [possible]
 
 class Knight(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Knight", colour, coords)
+
+	def getMoves(self, board):
 		possibleDirections = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, -2), (-1, 2)]
-		self.possibleMoves = []
+		moves = []
 
 		for d in possibleDirections:
-			self.possibleMoves.append((self.coords[0] + d[0], self.coords[1] + d[1]))
+			possible = (self.coords[0] + d[0], self.coords[1] + d[1])
+			moves.append(possible)
+
+		return moves
 
 class Bishop(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Bishop", colour, coords)
 
-		self.possibleMoves = self.getDiagonalMoves()
+	def getMoves(self, board):
+		return self.getDiagonalMoves(board)
 
 class Rook(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Rook", colour, coords)
 
-		self.possibleMoves = self.getStraightMoves()
+	def getMoves(self, board):
+		return self.getStraightMoves(board)
 
 class Queen(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Queen", colour, coords)
 
-		self.possibleMoves = self.getStraightMoves() + self.getDiagonalMoves()
+	def getMoves(self, board):
+		return self.getStraightMoves(board) + self.getDiagonalMoves(board)
 
 class King(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("King", colour, coords)
 
-		self.possibleMoves = self.getStraightMoves(limit=1) + self.getDiagonalMoves(limit=1)
+	def getMoves(self, board):
+		return self.getStraightMoves(board, limit=1) + self.getDiagonalMoves(board, limit=1)
