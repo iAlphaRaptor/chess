84b0ed14d5661069585a30809be4eab73bb8973d
iAlphaRaptor <maximwaring@yahoo.co.uk>
2021-02-06 10:38:31 +0000

    Generate possible moves for each piece

--- main.py
+++ main.py
@@ -8,10 +8,11 @@ clock = pygame.time.Clock()
 playing = True
 
 piecesGroup = pygame.sprite.Group()
-piecesGroup.add(piece.Pawn("black", (0, 0)),
+piecesGroup.add(piece.Pawn("white", (0, 3)),
 				piece.Rook("white", (2, 5)),
-				piece.King("black", (0, 7)),
-				piece.Queen("white", (4, 2)))
+				piece.King("black", (3, 3)),
+				piece.Queen("white", (4, 2)),
+				piece.Knight("black", (3, 5)))
 
 while playing:
 	for event in pygame.event.get():
@@ -29,6 +30,8 @@ while playing:
 	for i in range(1, 33, 2):
 		pygame.draw.rect(screen, LIGHTCOLOUR, ((i%8)*SQUAREWIDTH, (i//8)*2*SQUAREWIDTH+SQUAREWIDTH, SQUAREWIDTH, SQUAREWIDTH))
 
+	for m in piecesGroup.sprites()[4].possibleMoves:
+		pygame.draw.rect(screen, (0, 0, 255), (m[0]*SQUAREWIDTH, m[1]*SQUAREWIDTH, 10, 10))
 	piecesGroup.draw(screen)
 	pygame.display.flip()
 	clock.tick(60)
--- piece.py
+++ piece.py
@@ -28,36 +28,93 @@ class Piece(pygame.sprite.Sprite):
 	def __init__(self, piece, colour, coords):
 		super().__init__()
 
+		self.colour = colour
+		self.coords = coords
+
 		self.image = pygame.Surface([SQUAREWIDTH, SQUAREWIDTH])
 		self.image.set_colorkey((2, 2, 2))
 		self.image.fill((2, 2, 2))
 
 		self.rect = self.image.get_rect()
-		self.rect.x = coords[0] * SQUAREWIDTH
-		self.rect.y = coords[1] * SQUAREWIDTH
+		self.rect.x = self.coords[0] * SQUAREWIDTH
+		self.rect.y = self.coords[1] * SQUAREWIDTH
 
 		self.image.blit(blackPieces[piece] if colour == "black" else whitePieces[piece], (0,0))
+
+	def getStraightMoves(self, limit=False):
+		moves = []
+		if not limit:
+			for x in range(0, 8):
+				if x != self.coords[0]:
+					moves.append((x, self.coords[1]))
+			for y in range(0, 8):
+				if y != self.coords[1]:
+					moves.append((self.coords[0], y))
+		else:
+			for x in range(self.coords[0]-limit, self.coords[0]+limit+1):
+				if x != self.coords[0]:
+					moves.append((x, self.coords[1]))
+			for y in range(self.coords[1]-limit, self.coords[1]+limit+1):
+				if y != self.coords[1]:
+					moves.append((self.coords[0], y))
+
+		return moves
+
+	def getDiagonalMoves(self, limit=False):
+		moves = []
+		if not limit:
+			for i in range(1, min([7-self.coords[0], 7-self.coords[1]]) + 1):
+				moves.append((self.coords[0] + i, self.coords[1] + i))
+			for i in range(1, min([7-self.coords[0], self.coords[1]]) + 1):
+				moves.append((self.coords[0] + i, self.coords[1] - i))
+			for i in range(1, min([self.coords[0], self.coords[1]]) + 1):
+				moves.append((self.coords[0] - i, self.coords[1] - i))
+			for i in range(1, min([self.coords[0], 7-self.coords[1]]) + 1):
+				moves.append((self.coords[0] - i, self.coords[1] + i))
+		else:
+			dir = [(1,1), (-1,1), (1,-1), (-1,-1)]
+			for i in range(4):
+				for j in range(1, limit+1):
+					moves.append((self.coords[0]+(dir[i][0]*j), (self.coords[1]+(dir[i][1]*j))))
+
+		return moves
+
 
 class Pawn(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Pawn", colour, coords)
 
+		self.possibleMoves = [(self.coords[0], self.coords[1] + 1 if self.colour == "black" else self.coords[1] - 1)]
+
 class Knight(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Knight", colour, coords)
+		possibleDirections = [(2, 1), (2, -1), (-2, 1), (-2, -1), (1, 2), (1, -2), (-1, -2), (-1, 2)]
+		self.possibleMoves = []
+
+		for d in possibleDirections:
+			self.possibleMoves.append((self.coords[0] + d[0], self.coords[1] + d[1]))
 
 class Bishop(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Bishop", colour, coords)
 
+		self.possibleMoves = self.getDiagonalMoves()
+
 class Rook(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Rook", colour, coords)
+
+		self.possibleMoves = self.getStraightMoves()
 
 class Queen(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("Queen", colour, coords)
 
+		self.possibleMoves = self.getStraightMoves() + self.getDiagonalMoves()
+
 class King(Piece):
 	def __init__(self, colour, coords):
 		super().__init__("King", colour, coords)
+
+		self.possibleMoves = self.getStraightMoves(limit=1) + self.getDiagonalMoves(limit=1)
